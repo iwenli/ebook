e64a584fdd7b298b88c27e90120a6674e56cd530
iwenli <admin@iwenli.org>
2020-12-09 21:54:44 +0800

    新笔趣阁获取书籍信息

--- spider/fetchers/xbqg.py
+++ spider/fetchers/xbqg.py
@@ -5,7 +5,7 @@ License: Copyright © 2020 iwenli.org Inc. All rights reserved.
 Github: https://github.com/iwenli
 Date: 2020-12-04 15:52:07
 LastEditors: iwenli
-LastEditTime: 2020-12-05 21:51:55
+LastEditTime: 2020-12-09 21:53:07
 Description: 新笔趣阁
 '''
 __author__ = 'iwenli'
@@ -15,6 +15,41 @@ sys.path.append(os.path.abspath("."))
 from utils.network import Http
 
 http = Http(False)
+
+
+def get_book(book_name):
+    """抓取书籍信息
+
+    Args:
+        book_name ([string]): [书籍名称]
+    """
+    url = 'http://www.xbiquge.la/modules/article/waps.php?searchkey=' + book_name
+
+    # 搜索书籍
+    sp = http.get_beautifulsoup(url)
+    tag_a = sp.find('a', text=book_name)
+    if (tag_a is None):
+        return
+
+    book_url = tag_a.get('href')
+    book_sp = http.get_beautifulsoup(book_url)
+    main = book_sp.find("div", id="maininfo")
+    author = main.find("p").string.split("：")[1]
+    category = book_sp.find("div", class_="con_top").find_all("a")[2].string
+    cover = book_sp.find("div", id="fmimg").find("img").get('src')
+    desc = book_sp.find("div", id="intro").find_all("p")[1].string
+    book = {
+        'name': book_name,
+        'author': author,
+        'category': category,
+        'subcategory': None,
+        'rate': 0,
+        'cover': cover,
+        'status': None,
+        'wordNums': None,
+        'desc': desc
+    }
+    return book
 
 
 def get_chapters(book_name):
@@ -72,5 +107,5 @@ def get_chapter_content(chapter):
 
 
 if __name__ == "__main__":
-    chapters = get_chapters('斗罗大陆4终极斗罗')
-    print(chapters)
+    # print(get_chapters('斗罗大陆4终极斗罗'))
+    print(get_book('不败战神杨辰'))
