854a58f91e146cea9f494702fe8a22ed3bdc9977
iwenli <iwenli@IWENLI-UB>
2020-12-18 17:42:29 +0800

    miniApp init

---
+++ miniApp/ebook/pages/index/index.js
@@ -0,0 +1,18 @@
+Page({
+  data: {
+    PageCur: 'city',
+    ActionColor:'text-orange'
+  },
+  NavChange(e) {
+    this.setData({
+      PageCur: e.currentTarget.dataset.cur
+    })
+  },
+  onShareAppMessage() {
+    return {
+      title: 'ColorUI-高颜值的小程序UI组件库',
+      imageUrl: '/images/share.jpg',
+      path: '/pages/index/index'
+    }
+  },
+})
---
+++ miniApp/ebook/readme.md
@@ -0,0 +1,6 @@
+# 依赖
+> https://github.com/siwei-lu/minapp-iconfont
+
+
+# 配置
++ 添加域名 
